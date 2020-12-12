1a51369f9c5070250457bcaf694317523466a11a
Eugenia Lenti <evgn@outlook.com.gr>
2020-12-13 01:12:25 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script>window.appUrl = '{{ config("app.url") }}';</script>
+
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+</head>
+<body>
+<div id="app">
+
+    <main>
+        @yield('content')
+    </main>
+
+
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    HOME SWEET HOME
+
+
+@endsection
+
