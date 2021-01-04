f12cf2bd502adb23e93997d721d7042eb4684ff7
Eugenia Lenti <evgn@outlook.com.gr>
2021-01-04 09:32:34 +0200

    wip refactors create classroom

---
+++ resources/js/components/Classrooms/ActiveVisibleToggle.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ActiveVisibleToggle"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Courses/CourseToggle.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CourseToggle"
+}
+</script>
+
+<style scoped>
+
+</style>
