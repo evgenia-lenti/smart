364414a46d299f0ae383e2cbe5b1830be03aeffc
Theophilos Katsigiannis <tkatsigiannis@gmail.com>
2021-03-27 19:53:00 +0200

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -1950,8 +1950,8 @@ __webpack_require__.r(__webpack_exports__);
             classroom_id: this.classroom_id
         })
             .then((response) => {
-                  console.log('entered')
-              })
+                 console.log('entered')
+             })
             .catch((error) => {
             });
     */
@@ -2603,8 +2603,8 @@ __webpack_require__.r(__webpack_exports__);
             classroom_id: this.classroom_id
         })
             .then((response) => {
-                  console.log('entered')
-              })
+                 console.log('entered')
+             })
             .catch((error) => {
             });
     */
@@ -89406,8 +89406,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ducks\Desktop\code\smart\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ducks\Desktop\code\smart\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! /root/volume/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /root/volume/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
